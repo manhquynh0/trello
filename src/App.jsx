820099215eeead62e012ba2a496c256eb2dc7117
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-07-17 20:33:20 +0700

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css'
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <div>manhquynhdev</div>
+      <Typography variant ="body2" sx={{ color: 'text.secondary' }}>ManhQuynhDev - 1 lap trinh vien </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <CssBaseline />
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
