e64213fb743270605e1373b22cbc778562d5129b
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-07-18 21:43:41 +0700

    update theme dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+
 function App() {
   return (
     <>
       <div>manhquynhdev</div>
-      <Typography variant ="body2" sx={{ color: 'text.secondary' }}>ManhQuynhDev - 1 lap trinh vien </Typography>
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>ManhQuynhDev - 1 lap trinh vien </Typography>
+      <ModeToggle />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
