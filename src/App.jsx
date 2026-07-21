f51e4e4abfb8aadef6296b4dd53894fcc21680ce
Vũ Mạnh Quỳnh <vumanhquynh2852005@gmail.com>
2026-07-22 05:10:49 +0700

    update button lightdark mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,63 @@ import Typography from '@mui/material/Typography'
 import {
   useColorScheme
 } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import ToggleButton from '@mui/material/ToggleButton'
 
-function ModeToggle() {
+import IconButton from '@mui/material/IconButton'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import NightsStayRoundedIcon from '@mui/icons-material/NightsStayRounded'
+
+
+function ThemeSwitcher() {
   const { mode, setMode } = useColorScheme()
+  const handleToggleTheme = () => {
+    setMode(mode === 'dark' ? 'light' : 'dark')
+  }
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <IconButton onClick={handleToggleTheme} color="inherit">
+      {mode === 'dark' ? <LightModeIcon /> : <NightsStayRoundedIcon />}
+    </IconButton>
   )
 }
+
+// function ModeButtonToggle() {
+//   const { mode, setMode } = useColorScheme()
+//   const handleFormat = (event) => {
+//     setMode(event.target.value)
+//   }
+
+//   return (
+//     <ToggleButtonGroup
+//       onChange={handleFormat}
+//       aria-label="text formatting"
+//       value="mode"
+//     >
+//       <ToggleButton value="light" aria-label="bold">Light
+//       </ToggleButton>
+//       <ToggleButton value="dark" aria-label="italic">Dark
+//       </ToggleButton>
+//       <ToggleButton value="system" aria-label="underlined">System
+//       </ToggleButton>
+//     </ToggleButtonGroup>
+//   )
+// }
+// function ModeToggle() {
+//   const { mode, setMode } = useColorScheme()
+//   // const preferDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+//   // const preferLightMode = useMediaQuery('(prefers-color-scheme: light)')
+
+//   // return (
+//   //   <Button
+//   //     onClick={() => {
+//   //       setMode(mode === 'light' ? 'dark' : 'light')
+//   //     }}
+//   //   >
+//   //     {mode === 'light' ? 'Turn dark' : 'Turn light'}
+//   //   </Button>
+//   // )
+// }
 
 
 function App() {
@@ -27,7 +71,9 @@ function App() {
     <>
       <div>manhquynhdev</div>
       <Typography variant="body2" sx={{ color: 'text.secondary' }}>ManhQuynhDev - 1 lap trinh vien </Typography>
-      <ModeToggle />
+      {/* <ModeToggle /> */}
+      {/* <ModeButtonToggle /> */}
+      <ThemeSwitcher />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,21 @@
-import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import {
+  experimental_extendTheme as extendTheme
+} from '@mui/material/styles'
 // Create a theme instance.
 const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        // primary: {
-        //   // main: '#ff5252'
-        // }
+        primary: {
+          main: '#1976d2'
+        }
       }
     },
     dark: {
       palette: {
-        // primary: {
-        //   // main: '#000'
-        // }
+        primary: {
+          main: '#90caf9'
+        }
       }
     }
   }
